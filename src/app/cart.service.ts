d88cc357da3eba7b84d2ca260c1a9a49e3f99997
fratardi <fratardi@student.42.fr>
2020-10-30 10:41:37 +0100

    4 files modified, 3 added cart service and checkout feature

---
+++ src/app/cart.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({ 
+  provideIn: 'root'
+  })
+export class CartService {
+  items = [];
+  addToCart(product) 
+  {
+    this.items.push(product);
+  }
+
+  getItems() 
+  {
+    return this.items;
+  }
+  clearCart() 
+  {
+    this.items = [];
+    return this.items;
+  }
+
+
+}
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 
 import { products } from "../products";
-
+import { CartService } from '../cart.service';
 
 
 @Component({
@@ -13,8 +13,14 @@ import { products } from "../products";
 export class ProductDetailsComponent implements OnInit {
  product;
   
+ addToCart(product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
+  }
+
   constructor(
     private route: ActivatedRoute,
+    private cartService: CartService
     )   {'productId'};
 
   ngOnInit() {
